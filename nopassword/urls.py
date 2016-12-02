fe95b2ab35ff835b26b7512f7a4e6acb8b89f5a8
Donny Winston <dwinston@alum.mit.edu>
2016-12-01 22:51:55 -0800

    Use django's own regex for username checking
    See https://github.com/django/django/blob/1.8.17/django/contrib/auth/models.py#L389

--- nopassword/urls.py
+++ nopassword/urls.py
@@ -1,4 +1,4 @@
-# -*- coding: utf8 -*-
+# coding: utf8
 from django.conf.urls import url
 from nopassword.views import login, login_with_code, \
     login_with_code_and_username, logout
@@ -7,7 +7,7 @@ from nopassword.views import login, login_with_code, \
 urlpatterns = [
     url(r'^login/$', login, name='login'),
     url(r'^login-code/(?P<login_code>[a-zA-Z0-9]+)/$', login_with_code),
-    url(r'^login-code/(?P<username>[a-zA-Z0-9_@\.\+-]+)/(?P<login_code>[a-zA-Z0-9]+)/$',
+    url(r'^login-code/(?P<username>[\w.@+-]+)/(?P<login_code>[a-zA-Z0-9]+)/$',
         login_with_code_and_username),
     url(r'^logout/$', logout),
 ]
